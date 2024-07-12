250626814c5c3c331f1ac03f4ca13f77915c9a9c
navin136 <nkwhitehat@gmail.com>
2024-07-12 19:46:27 +0530

    FTH --> MeltEase and some more changes to make attractive

--- js/spec.js
+++ js/spec.js
@@ -1,5 +1,5 @@
 function fetch(ptn,a){      // Don't touch these lines
-    let pattern = ptn;      // Don't touch these lines
+let pattern=ptn;      // Don't touch these lines
 
 
 // Master data Starts here
@@ -19,6 +19,22 @@ if(pattern == '7654'){
     var temperature = '1400-1415';
     var taddition = 'NA';
     var mtwt = 1500;
+}
+if(pattern == '7297'){
+    var pcarbon = 3.25;
+    var psilicon = 3.85;
+    var pcopper = 0.08;
+    var ptin = 0.008;
+    var pmanganese = 0.28;
+    var pmolybdenum = 0.55;
+    var pnickel = 0.01;
+    var pmagnesium = 0.035;
+    var laddition = 'NA';
+    var inoculant = 'Zircseed';
+    var flowrate = 3.3;
+    var temperature = '1390-1405';
+    var taddition = '0.05% Superseed';
+    var mtwt = 1700;
 }
 //7470-5
 if(pattern == '7470-5'){
@@ -46,6 +62,7 @@ if(pattern == '7278'){
     var pmanganese = 0.28;
     var pmolybdenum = 0.55;
     var pnickel = 0.58;
+    var ptitanium = 0.07;
     var pmagnesium = 0.015;
     var laddition = '0.05% Fesi Granules';
     var inoculant = 'Fesi';
@@ -57,16 +74,57 @@ if(pattern == '7278'){
 //some useful functions
 
 if(pcopper>=0.20){
-    var grade = 'sg-copper'
+    var grade = 'sg-copper';
+    // document.querySelector("#lptitanium").style.visibility = 'hidden';
+    // document.querySelector("#ptitanium").style.visibility = 'hidden';
+    // document.querySelector("#lpmolybdenum").style.visibility = 'hidden';
+    // document.querySelector("#pmolybdenum").style.visibility = 'hidden';
+    // document.querySelector(".niclass").style.visibility = 'hidden';
+    // document.querySelector(".moclass").style.visibility = 'hidden';
+    // document.querySelector(".ticlass").style.visibility = 'hidden';
 }
 if(ptin>=0.02){
-    var grade = 'sg-tin'
+    var grade = 'sg-tin';
+    
 }
 if(pcarbon>=3.88){
     var grade = 'sg-azterlan';
 }
 if(pnickel>0.5){
-    var grade = 'simoni'
+    var grade = 'simoni';
+    // document.querySelector("#lpcopper").style.visibility = 'hidden';
+    // document.querySelector("#pcopper").style.visibility = 'hidden';
+    // document.querySelector("#lptin").style.visibility = 'hidden';
+    // document.querySelector("#ptin").style.visibility = 'hidden';
+    // document.querySelector(".cuclass").style.visibility = 'hidden';
+    // document.querySelector(".snclass").style.visibility = 'hidden';
+    // document.querySelector(".mnclass").style.visibility = 'hidden';
+}
+if(pmolybdenum>0.3 && pmolybdenum<0.6 && pnickel<0.5){
+    var grade = 'sg-lowmoly';
+    // document.querySelector("#lpcopper").style.visibility = 'hidden';
+    // document.querySelector("#pcopper").style.visibility = 'hidden';
+    // document.querySelector("#lptin").style.visibility = 'hidden';
+    // document.querySelector("#ptin").style.visibility = 'hidden';
+    // document.querySelector("#lptitanium").style.visibility = 'hidden';
+    // document.querySelector("#ptitanium").style.visibility = 'hidden';
+    // document.querySelector(".ticlass").style.visibility = 'hidden';
+    // document.querySelector(".niclass").style.visibility = 'hidden';
+    // document.querySelector(".cuclass").style.visibility = 'hidden';
+    // document.querySelector(".snclass").style.visibility = 'hidden';
+}
+if(pmolybdenum>0.6 && pnickel<0.5){
+    var grade = 'sg-highmoly';
+    // document.querySelector("#lpcopper").style.visibility = 'hidden';
+    // document.querySelector("#pcopper").style.visibility = 'hidden';
+    // document.querySelector("#lptin").style.visibility = 'hidden';
+    // document.querySelector("#ptin").style.visibility = 'hidden';
+    // document.querySelector("#lptitanium").style.visibility = 'hidden';
+    // document.querySelector("#ptitanium").style.visibility = 'hidden';
+    // document.querySelector(".ticlass").style.visibility = 'hidden';
+    // document.querySelector(".niclass").style.visibility = 'hidden';
+    // document.querySelector(".cuclass").style.visibility = 'hidden';
+    // document.querySelector(".snclass").style.visibility = 'hidden';
 }
 if(pmagnesium>=0.03){
     var alloyp = 0.92;
@@ -85,7 +143,30 @@ if(a=='holding'){
     document.getElementById("ptin").value = ptin;
     document.getElementById("pmanganese").value = pmanganese;
     document.getElementById("pmolybdenum").value = pmolybdenum;
+    document.getElementById("ptitanium").value = ptitanium;
     document.getElementById("pnickel").value = pnickel;
+    if(grade == 'sg-tin' || grade == 'sg-copper'|| grade == 'sg-azterlan'){
+        document.querySelector(".niclass").style.visibility = 'hidden';
+        document.querySelector(".moclass").style.visibility = 'hidden';
+        document.querySelector(".ticlass").style.visibility = 'hidden';
+        document.querySelector("#lptitanium").style.visibility = 'hidden';
+        document.querySelector("#ptitanium").style.visibility = 'hidden';
+        document.querySelector("#lpnickel").style.visibility = 'hidden';
+        document.querySelector("#pnickel").style.visibility = 'hidden';
+        document.querySelector("#lpmolybdenum").style.visibility = 'hidden';
+        document.querySelector("#pmolybdenum").style.visibility = 'hidden';
+    }
+    if(grade == 'sg-highmoly' || grade == 'sg-lowmoly'){
+        document.querySelector(".mnclass").style.visibility = 'hidden';
+        document.querySelector(".mnclass").style.height = '0px';
+        document.querySelector(".niclass").style.visibility = 'hidden';
+        document.querySelector(".ticlass").style.visibility = 'hidden';
+        document.querySelector("#lptitanium").style.visibility = 'hidden';
+        document.querySelector("#ptitanium").style.visibility = 'hidden';
+        document.querySelector("#lpnickel").style.visibility = 'hidden';
+        document.querySelector("#pnickel").style.visibility = 'hidden';
+    }
+    
 }
 if(a=='treatment'){
     document.getElementById("pattern").value = pattern;
@@ -117,4 +198,5 @@ if(a=='melting'){
     document.getElementById("pmolybdenum").value = pmolybdenum;
     document.getElementById("pnickel").value = pnickel;
 }
+
 }
