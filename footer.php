280e326c3b9309064431d11213b5054105b596b4
navin136 <nkwhitehat@gmail.com>
2024-08-09 12:49:45 +0530

    Describe GNU license in footer

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 <body>
     <div id='footer'>
         <h2><b>Developed & Designed by Navin Kumar</b></h2>
-        <p>&#169 Copyright 2024. All rights reserved.</p>
+        <p>&#169 2024 Navin Kumar. Licensed under GNU GPL Version 3.0</p>
     </div>
 </body>
 </html>
