9176514168c106ef62f89cfeda3036c89f707f19
navin136 <nkwhitehat@gmail.com>
2024-08-15 22:10:49 +0530

    MeltEase: Moulding: Add plan updating form
    Signed-off-by: navin136 <nkwhitehat@gmail.com>

--- moulding.php
+++ moulding.php
@@ -60,38 +60,128 @@
         }
         }
     ?>
-    <!-- <div class="mouldingplan">
+    <?php
+        if(isset($_POST["date"]) && isset($_POST["plan1"]) && isset($_POST["plan1moulds"])){
+            $date = $_POST["date"];
+            $plan1 = $_POST["plan1"];
+            $plan2 = $_POST["plan2"];
+            $plan3 = $_POST["plan3"];
+            $plan4 = $_POST["plan4"];
+            $plan5 = $_POST["plan5"];
+            $plan6 = $_POST["plan6"];
+            $plan1moulds = $_POST["plan1moulds"];
+            $plan2moulds = $_POST["plan2moulds"];
+            $plan3moulds = $_POST["plan3moulds"];
+            $plan4moulds = $_POST["plan4moulds"];
+            $plan5moulds = $_POST["plan5moulds"];
+            $plan6moulds = $_POST["plan6moulds"];
+        }
+    ?>
+    <div class="mouldingplan">
+        <form action="moulding.php" method="post">
         <div class="date">
             <label for="date">Date</label>
             <input type="date" name="date" id="date">
         </div>
         <div class="plan1">
             <label for="plan1">Plan 1</label>
-            <input type="text" id="plan1" name="plan1" placeholder="Part Number">
+            <select name="plan1">
+            <option value=""></option>
+                <?php
+                $sql="select part_number from part_details";
+                $result = $conn->query($sql);
+                if($result->num_rows>0){
+                    while($row = $result->fetch_assoc()){
+                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
+                    }
+                }
+                ?>
+            </select>
             <input type="text" name="plan1moulds" id="plan1moulds" placeholder="No. Of Moulds">
         </div>
         <div class="plan2">
             <label for="plan2">Plan 2</label>
-            <input type="text" id="plan2" name="plan2" placeholder="Part Number">
+            <select name="plan2">
+            <option value=""></option>
+                <?php
+                $sql="select part_number from part_details";
+                $result = $conn->query($sql);
+                if($result->num_rows>0){
+                    while($row = $result->fetch_assoc()){
+                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
+                    }
+                }
+                ?>
+            </select>
             <input type="text" name="plan2moulds" id="plan2moulds" placeholder="No. Of Moulds">
         </div>
         <div class="plan3">
             <label for="plan3">Plan 3</label>
-            <input type="text" id="plan3" name="plan3" placeholder="Part Number">
+            <select name="plan3">
+            <option value=""></option>
+                <?php
+                $sql="select part_number from part_details";
+                $result = $conn->query($sql);
+                if($result->num_rows>0){
+                    while($row = $result->fetch_assoc()){
+                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
+                    }
+                }
+                ?>
+            </select>
             <input type="text" name="plan3moulds" id="plan3moulds" placeholder="No. Of Moulds">
         </div>
         <div class="plan4">
             <label for="plan4">Plan 4</label>
-            <input type="text" id="plan4" name="plan4" placeholder="Part Number">
+            <select name="plan4">
+            <option value=""></option>
+                <?php
+                $sql="select part_number from part_details";
+                $result = $conn->query($sql);
+                if($result->num_rows>0){
+                    while($row = $result->fetch_assoc()){
+                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
+                    }
+                }
+                ?>
+            </select>
             <input type="text" name="plan4moulds" id="plan4moulds" placeholder="No. Of Moulds">
         </div>
         <div class="plan5">
             <label for="plan5">Plan 5</label>
-            <input type="text" id="plan5" name="plan5" placeholder="Part Number">
+            <select name="plan5">
+            <option value=""></option>
+                <?php
+                $sql="select part_number from part_details";
+                $result = $conn->query($sql);
+                if($result->num_rows>0){
+                    while($row = $result->fetch_assoc()){
+                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
+                    }
+                }
+                ?>
+            </select>
             <input type="text" name="plan5moulds" id="plan5moulds" placeholder="No. Of Moulds">
         </div>
-        <button type="submit">Upload</button>
-    </div> -->
+        <div class="plan6">
+            <label for="plan6">Plan 6</label>
+            <select name="plan6">
+            <option value=""></option>
+                <?php
+                $sql="select part_number from part_details";
+                $result = $conn->query($sql);
+                if($result->num_rows>0){
+                    while($row = $result->fetch_assoc()){
+                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
+                    }
+                }
+                ?>
+            </select>
+            <input type="text" name="plan6moulds" id="plan6moulds" placeholder="No. Of Moulds">
+        </div>
+        <button type="submit">Submit</button>
+        </form>
+    </div>
     <?php   
     include("footer.php");
     ?>
