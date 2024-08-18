aa75ae69de45893b8e2c54f9550d891a0aa8b159
navin136 <nkwhitehat@gmail.com>
2024-08-19 01:51:49 +0530

    Meltease: Change title of authenticate page
    Signed-off-by: navin136 <nkwhitehat@gmail.com>

--- authenticate.php
+++ authenticate.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Edit master Data</title>
+    <title>Login to Home</title>
     <link rel="stylesheet" href="css/authenticate.css">
     <link rel='icon' href='icon.png'>
 </head>
