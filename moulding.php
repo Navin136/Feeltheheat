6bce81a60dba2af35c9dc5b9516d123a7d3b1ac0
navin136 <nkwhitehat@gmail.com>
2024-08-18 07:58:07 +0530

    Meltease: Moulding: Add option to show next pattern
    Would be useful for holder chemistry adjustment and melting furnace chargemix suggestion

--- moulding.php
+++ moulding.php
@@ -14,6 +14,8 @@
     <?php   
     include("header.php");
     ?>
+    <div id="boxes">
+    <div class="box1">
     <div id="form">
     <form action="moulding.php" method="post">
         <label for="current_part">Please Enter Running part      </label>
@@ -55,36 +57,50 @@
                 }
             }
         }
+        }
         else{
             echo "<p id='warning'>Please choose part number carefully, This change will reflect everywhere</p>";
         }
-        }
     ?>
     <?php
-        if(isset($_POST["date"]) && isset($_POST["plan1"]) && isset($_POST["plan1moulds"])){
-            $date = $_POST["date"];
-            $plan1 = $_POST["plan1"];
-            $plan2 = $_POST["plan2"];
-            $plan3 = $_POST["plan3"];
-            $plan4 = $_POST["plan4"];
-            $plan5 = $_POST["plan5"];
-            $plan6 = $_POST["plan6"];
-            $plan1moulds = $_POST["plan1moulds"];
-            $plan2moulds = $_POST["plan2moulds"];
-            $plan3moulds = $_POST["plan3moulds"];
-            $plan4moulds = $_POST["plan4moulds"];
-            $plan5moulds = $_POST["plan5moulds"];
-            $plan6moulds = $_POST["plan6moulds"];
+        if(isset($_POST["plan1"])){
+            if($_POST["plan1"] != ''){
+                $plan1 = $_POST["plan1"];
+                $nk = "select * from live where id=2";
+                $result = $conn->query($nk);
+                if($result->num_rows>0){
+                    $nextplan = "UPDATE live set current_part=$plan1 where id=2";
+                    $nextplanpushed = $conn->query($nextplan);
+                    if($nextplanpushed){
+                        echo"<p class='success'>Next plan Updated to database</p>";
+                    }
+                    else{
+                         echo "<p id='warning'>Failed to update database</p>";
+                    }
+            }else{
+                $nextplan = "INSERT INTO live(current_part,id) VALUES ('$plan1','2')";
+                $nextplanpushed = $conn->query($nextplan);
+                if($nextplanpushed){
+                    echo"<p class='success'>Next plan Pushed to database</p>";
+                }
+                else{
+                     echo "<p id='warning'>Failed to update database</p>";
+                }
+            }
+            }
+            else{
+                echo "<p id='warning'>Please Give next pattern to process</p>";
+            }
         }
+        else{
+            echo "<p id='warning'>Please Give next pattern to process</p>";
+        }
+            
     ?>
     <div class="mouldingplan">
         <form action="moulding.php" method="post">
-        <div class="date">
-            <label for="date">Date</label>
-            <input type="date" name="date" id="date">
-        </div>
         <div class="plan1">
-            <label for="plan1">Plan 1</label>
+            <label for="plan1">Next Pattern</label>
             <select name="plan1">
             <option value=""></option>
                 <?php
@@ -96,91 +112,23 @@
                     }
                 }
                 ?>
-            </select>
-            <input type="text" name="plan1moulds" id="plan1moulds" placeholder="No. Of Moulds">
+        </select>
+        <button type="submit" id="npbtn">Submit</button>    
         </div>
-        <div class="plan2">
-            <label for="plan2">Plan 2</label>
-            <select name="plan2">
-            <option value=""></option>
-                <?php
-                $sql="select part_number from part_details";
-                $result = $conn->query($sql);
-                if($result->num_rows>0){
-                    while($row = $result->fetch_assoc()){
-                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
-                    }
-                }
-                ?>
-            </select>
-            <input type="text" name="plan2moulds" id="plan2moulds" placeholder="No. Of Moulds">
-        </div>
-        <div class="plan3">
-            <label for="plan3">Plan 3</label>
-            <select name="plan3">
-            <option value=""></option>
-                <?php
-                $sql="select part_number from part_details";
-                $result = $conn->query($sql);
-                if($result->num_rows>0){
-                    while($row = $result->fetch_assoc()){
-                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
-                    }
-                }
-                ?>
-            </select>
-            <input type="text" name="plan3moulds" id="plan3moulds" placeholder="No. Of Moulds">
-        </div>
-        <div class="plan4">
-            <label for="plan4">Plan 4</label>
-            <select name="plan4">
-            <option value=""></option>
-                <?php
-                $sql="select part_number from part_details";
-                $result = $conn->query($sql);
-                if($result->num_rows>0){
-                    while($row = $result->fetch_assoc()){
-                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
-                    }
-                }
-                ?>
-            </select>
-            <input type="text" name="plan4moulds" id="plan4moulds" placeholder="No. Of Moulds">
-        </div>
-        <div class="plan5">
-            <label for="plan5">Plan 5</label>
-            <select name="plan5">
-            <option value=""></option>
-                <?php
-                $sql="select part_number from part_details";
-                $result = $conn->query($sql);
-                if($result->num_rows>0){
-                    while($row = $result->fetch_assoc()){
-                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
-                    }
-                }
-                ?>
-            </select>
-            <input type="text" name="plan5moulds" id="plan5moulds" placeholder="No. Of Moulds">
-        </div>
-        <div class="plan6">
-            <label for="plan6">Plan 6</label>
-            <select name="plan6">
-            <option value=""></option>
-                <?php
-                $sql="select part_number from part_details";
-                $result = $conn->query($sql);
-                if($result->num_rows>0){
-                    while($row = $result->fetch_assoc()){
-                        echo"<option value=\"$row[part_number]\">$row[part_number]</option>";
-                    }
-                }
-                ?>
-            </select>
-            <input type="text" name="plan6moulds" id="plan6moulds" placeholder="No. Of Moulds">
-        </div>
-        <button type="submit">Submit</button>
         </form>
+    </div>
+    </div>
+    <div class="box2">
+            <?php
+            $cp="SELECT current_part from live where id=1";
+            $np="SELECT current_part from live where id=2";
+            $cpr = $conn->query($cp);
+            $cpp = $cpr->fetch_assoc();
+            $npr = $conn->query($np);
+            $npp = $npr->fetch_assoc();
+            echo '<p>Current pattern :</br><b>'.$cpp["current_part"].'</b></br> Next Pattern :</br><b>'.$npp["current_part"].'</b></p>';
+            ?>
+    </div>
     </div>
     <?php   
     include("footer.php");
