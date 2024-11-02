3055d623a7e1a2ce65fc6560319f2a74c360a988
navin136 <nkwhitehat@gmail.com>
2024-11-02 22:13:50 +0530

    Meltease: Add Material planner
    Signed-off-by: navin136 <nkwhitehat@gmail.com>

--- rawmaterial.php
+++ rawmaterial.php
@@ -19,8 +19,44 @@
         if(isset($_POST['get_material'])){
             $part_no = $_POST['part_no'];
             $moulds = $_POST['moulds'];
-            $treewt_query = "select tree_weight from part_details where part_number='$part_no'";
-            $treewt_result = $conn->query($treewt_query);
+            $get_part = "SELECT * FROM part_details WHERE part_number=$part_no";
+            $get_pert_result = $conn->query($get_part);
+            if($get_pert_result->num_rows>0){
+                $row=$get_pert_result->fetch_assoc();
+                $treewt_db = $row['treewt'];
+                $inoculant = $row['inoculant'];
+                $alloy = $row['alloy'];
+                $mtwt = $row['metalweight'];
+                $fr = $row['flow_rate'];
+                $laddition = $row['laddition'];
+                $taddition = $row['taddition'];
+                $mischmetal = $row['mischmetal'];
+                $copper = $row['copper'];
+                $tin = $row['tin'];
+                $manganese = $row['manganese'];
+                $magnesium = $row['magnesium'];
+                $pouring_time = $row['pouring_time'];
+                $trim_time = (int)substr($pouring_time,0,-3);
+                $flow_rate = (int)$row['flow_rate'];
+                $metal_needed = ($treewt_db * $moulds)/1000;
+                echo "The needed metal for the plan of '$part_no' for '$moulds' moulds is '$metal_needed' tons"."</br>";
+                $inoculant_required = ($flow_rate*$moulds*$trim_time)/1000;
+                echo "Inoculant needed is $inoculant_required Kgs of ".$inoculant."</br>";
+                $moulds_per_ladle = $mtwt/$treewt_db;
+                echo "Moulds per ladle is ".(int)$moulds_per_ladle . "   [ ".$mtwt." Kgs ]"."</br>";
+                $number_of_ladles=(int)($moulds/$moulds_per_ladle);
+                echo "Number of Ladles Required is ".$number_of_ladles."</br>";
+                if($magnesium > 0.02){
+                    $alloy_needed = $number_of_ladles * 0.92 * $mtwt / 100;
+                    echo "Alloy needed is $alloy_needed Kgs of $alloy </br>";
+                }else{
+                    $alloy_needed = $number_of_ladles * 0.3 * $mtwt / 100;
+                    echo "Alloy needed is $alloy_needed Kgs of $alloy </br>";
+                }
+                
+            }else{
+                echo "Query failed";
+            }
         }
     ?>
     <form action="rawmaterial.php" method="post">
@@ -38,7 +74,7 @@
                 ?>
         </select>
         <label for="moulds">No. of Moulds</label>
-        <input type="text" name="moulds">
+        <input name="moulds">
         <button type="submit" name="get_material">Get Required material</button>
     </form>
     <?php   
