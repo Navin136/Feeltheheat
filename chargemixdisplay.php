e775edc9b609340a2787bc621a0d2e28341182e6
navin136 <nkwhitehat@gmail.com>
2024-11-27 06:39:59 +0530

    Meltease: AChargemidisplay: auto reefresh every 5 seconds
    Signed-off-by: navin136 <nkwhitehat@gmail.com>

--- chargemixdisplay.php
+++ chargemixdisplay.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset='UTF-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-	<!-- <meta http-equiv="refresh" content="10"> -->
+    	<meta http-equiv="refresh" content="5">
 	<title>Chargemix</title>
 	<link rel="stylesheet" href="css/chargemixdisplay.css">
 </head>
